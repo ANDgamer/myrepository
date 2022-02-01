2923cbbe1104d1a9b7f167af19ee594606f90963
ANDgamer <andriychernysh88@gmail.com>
2022-02-01 18:56:42 +0200

    add files

---
+++ main.js
@@ -0,0 +1,19 @@
+const btns = [".fa-bars", ".fa-plus"]
+const menus = [".menu__wrapper", ".plus__wrapper"]
+
+btns.forEach((i, n) => {
+	let btn = document.querySelector(i)
+	btn.addEventListener("click", () => {
+		let menu = document.querySelector(menus[n])
+
+		menus.forEach(s => {
+			let iMenu = document.querySelector(s)
+
+			if (iMenu.classList.contains("open") && s !== menus[n]) {
+				iMenu.classList.remove("open")
+			}
+		})
+
+		menu.classList.toggle("open")
+	})
+})
